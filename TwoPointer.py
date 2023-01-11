0713dc4ccbc27191d1be9f6bb5c42589fd3c80de
sayeedk06 <sayeed.k06@gmail.com>
2023-01-11 18:36:37 +0600

    Two pointer - Remove Duplicates and Remove Key Instance -done

--- TwoPointer.py
+++ TwoPointer.py
@@ -1,8 +1,11 @@
 class TwoPointer:
-    def __init__(self, givenInput, target) -> None:
+    def __init__(self, givenInput, target=None) -> None:
         self.target = target
         self.givenInput = givenInput
     def PairwithTargetSum(self):
+
+        # Time Complexity = O(N)
+        # Space Complexity = O(1)
         start = 0
         end = len(self.givenInput)-1
         sum = 0
@@ -17,11 +20,24 @@ class TwoPointer:
         
         return "No possible solution is present"
 
+    def RemoveDuplicate(self):
+
+        # Time Complexity = O(N)
+        # Space Complexity = O(1)
+        i = 0
+        non_duplicate = 1
+
+        while(i < len(self.givenInput)):
+            if self.givenInput[non_duplicate - 1] != self.givenInput[i]:
+                self.givenInput[non_duplicate] = self.givenInput[i]
+                non_duplicate += 1
+            i += 1
+        return non_duplicate
 
 x = list(map(int, input('Enter the given input -> ').split()))
-y = int(input('Enter the target value - > '))
-test = TwoPointer(x,y)
-
-print(test.PairwithTargetSum())
+# y = int(input('Enter the target value - > '))
+test = TwoPointer(x)
+# print(test.PairwithTargetSum())
+print(test.RemoveDuplicate())
 
     
